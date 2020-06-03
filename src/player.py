a5f2bc12cf9e1a73048c5a7473d448bba6129f13
Devin_Moore <d.j.moore209@gmail.com>
2020-06-03 15:57:15 -0400

    created some outlines of player and room

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, room):
+        self.room = room
+
+    def here(self):
+        return f'I am in the {self.room} room'
+    
+    def move(self, direction):
+        
